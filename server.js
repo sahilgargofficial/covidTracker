a1170c0431d26ce1befe01986c7e870458babf2b
sahilgargofficial <sahilgargofficial@gmail.com>
2020-08-17 20:01:19 +0530

    fix

--- server.js
+++ server.js
@@ -5,4 +5,4 @@ app.use(express.static(__dirname + '/dist/covidtracker'))
 app.get('/*' , (req , res )=>{
     res.sendFile(path.join(__dirname + '/dist/covidtracker/index.html'));
 });
-app.listen(8000)
+app.listen(process.env.PORT|8000)
